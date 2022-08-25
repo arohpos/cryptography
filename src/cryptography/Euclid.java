ad97dc7195a46ba6ec8bd3b44bbf528c04ff4d55
arohpos <110832649+arohpos@users.noreply.github.com>
2022-08-25 23:55:43 +0900

    add: 拡張したユークリッドのアルゴリズムの追加
    メソッドextgcdを追加し、mainから呼び出す

--- src/cryptography/Euclid.java
+++ src/cryptography/Euclid.java
@@ -17,21 +17,99 @@ public class Euclid{
 		
 		int r = 0;
 		
-		System.out.println("m:" + m);
-		System.out.println("n:" + n);
+		System.out.print("m = " + m + "、n = " + n + "のとき、");
 		
 		while(n!=0) {
 			
 			r = m % n;
 			m = n;
 			n = r;
-
+			
 		}
 		
 		System.out.println("最大公約数:" + m);
-		
 		return m;
+	}
+	
+	
+	/**
+	 * @param a メソッドの引数1
+	 * @param b メソッドの引数2
+	 * @return (x, y) s.t. ax + by = gcd(a, b) 
+	 */
+	
+	public int[] extgcd(int a, int b) {
+		
+		System.out.print("a = " + a + "、 b = " + b + "のとき、");
+		System.out.println("a * x + b * y = gcd(a, b)をみたす(x, y)をもとめる。");
 		
 		
+		//2つの解を配列を用いて戻り値とする	
+		int[] sols = new int[2];
+		
+		sols[0] = 1;
+		sols[1] = 2;
+		
+		int x_0 = 1;
+		int x_1 = 0;
+		int y_0 = 0;
+		int y_1 = 1;
+		int r_0 = a;
+		int r_1 = b;
+		
+		int sign = 1;
+		
+		System.out.println("t:\tx_t\ty_t\tq_t\tr_t");
+		System.out.println("0:\t1 \t0\t-\t" + a);
+		System.out.println("1:\t0\t1\t-\t" + b);
+		
+		//ループ数t（t=0, 1のケースはすでに出力されているため、t=2とする。）
+		int index = 2;
+		
+		boolean isEnd = false;
+		
+		while(isEnd == false) {
+			
+			int q = r_0 / r_1;
+			int r_2 = r_0 % r_1;
+			int x_2 = x_0 + q * x_1;
+			int y_2 = y_0 + q * y_1;
+			
+			System.out.println(index + ":\t" + x_2 + "\t" + y_2 + "\t" + q + "\t" + r_2);
+			
+			//r_tの更新（r_0が割られる数、r_1が割る数)
+			r_0 = r_1;
+			r_1 = r_2;
+			
+			//x_tとy_tの更新
+			x_0 = x_1;
+			x_1 = x_2;
+			y_0 = y_1;
+			y_1 = y_2;
+			
+			
+			//ループ終了条件
+			if(r_2 == 0) {
+				
+				isEnd = true;	
+				
+			}
+			
+			//ループ数更新
+			index = index + 1;
+			
+			//正負入替え
+			sign = -sign;
+			
+		}
+		
+		//解
+		sols[0] = sign * x_0;
+		sols[1] = -sign * y_0;
+				
+		System.out.print("以上の整理より、x = " + sols[0] + "、 y = " + sols[1]+ "のとき、");
+		System.out.println(a + " * " + sols[0] + " + " + b + " * " + sols[1]+ " = gcd(a, b)を満たす。");
+		
+		return sols;
 	}
 }
--- src/cryptography/Main.java
+++ src/cryptography/Main.java
@@ -5,10 +5,12 @@ public class Main{
 	public static void main(String[] args) {
 		
 		System.out.println("Hello, world");
-		
+				
 		Euclid euclid = new Euclid();
-			
+		
 		euclid.gcd(3, 1);		
 		
+		euclid.extgcd(100, 35);
+
 	}
 }
