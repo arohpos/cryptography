5b4fefcb2e75961c65ec4dff0c72ceda0fdad084
arohpos <110832649+arohpos@users.noreply.github.com>
2022-08-25 23:56:56 +0900

    add: メソッドextgcdの追加に伴うテストの加筆

--- src/cryptography/EuclidTest.java
+++ src/cryptography/EuclidTest.java
@@ -13,11 +13,43 @@ import org.junit.Test;
 
 public class EuclidTest{
 	
+	int a = 100;
+	int b = 35;
+	
+	//最大公約数が正しく求まるか
 	@Test
 	public void testGcd() {
 		Euclid euclid = new Euclid();
 		
-		assertEquals(3, euclid.gcd(12, 15));
+		assertEquals(5, euclid.gcd(a, b));
 		
 	}
+
+	//(a, b)に対してax * by = gcd(a, b)をみたすxが正しく求まるか
+	@Test
+	public void testExtgcdSol1() {
+		Euclid euclid = new Euclid();
+		
+		assertEquals(-1, euclid.extgcd(a, b)[0]);
+		
+	}
+	
+	//(a, b)に対してax * by = gcd(a, b)をみたすyが正しく求まるか
+	@Test
+	public void testExtgcdSol2() {
+		Euclid euclid = new Euclid();
+		
+		assertEquals(3, euclid.extgcd(a, b)[1]);
+		
+	}
+	
+	//(a, b)に対してax * by = gcd(a, b)をみたす(x, y)を計算し、正しく等式が満たされるか
+	@Test
+	public void testExtgcdEq() {
+		Euclid euclid = new Euclid();
+		
+		assertEquals(euclid.gcd(a, b), a * euclid.extgcd(a, b)[0] + b * euclid.extgcd(a, b)[1]);
+		
+	}
+	
 }
