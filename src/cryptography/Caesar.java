4dcfd6154f53891045981d7de4ee56034dc7ffa5
arohpos <110832649+arohpos@users.noreply.github.com>
2022-08-31 23:59:19 +0900

    add: add Caesar.java with Javadoc Comments

---
+++ src/cryptography/Caesar.java
@@ -0,0 +1,54 @@
+package cryptography;
+
+/**
+ * 
+ * @author arohpos
+ *
+ */
+
+public class Caesar{
+	
+	int mode = 1;
+	int N = 26;
+	
+	/* Want:
+	 * 現状はアルファベットは一般的なアルファベット（A, B, C, ..., Z）とする。（mode1）
+	 * いずれは、アルファベットの種類を増やし、選択したmodeによって変換方式が変更されるように実装する。
+	 */
+	
+	
+	/**
+	 * shift character once
+	 * @param ch	character
+	 * @return char + 1 mod N
+	 */
+	
+	
+	 /**
+	  * shit character multiple times
+	  * @param ch			character
+	  * @param shift_times	times to use cir_shitt method
+	  * @return ch + shift_times mod N
+	  */
+	
+	/**
+	 * encript plaintext into ciphertext by encription key
+	 * @param p			plaintext
+	 * @param enc_key	encription key
+	 * @return c		ciphertext
+	 */
+	
+	/**
+	 * generate decription key from encription key
+	 * @param enc_key 		encription key
+	 * @return dec_key 		decription key
+	 */
+	
+	/**
+	 * decript ciphertext into plaintext by decription key
+	 * @param c 			ciphertext
+	 * @param dec_key 		decription key
+	 * @return p 			plaintext
+	 */
+	
+}
