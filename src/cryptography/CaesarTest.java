985c3bcadbd7816d3d50d963fc153263503021b9
arohpos <110832649+arohpos@users.noreply.github.com>
2022-09-04 22:45:44 +0900

    add: add file "

---
+++ src/cryptography/CaesarTest.java
@@ -0,0 +1,47 @@
+package cryptography;
+
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author arohpos
+ *
+ */
+
+public class CaesarTest{
+	
+	int a = 100;
+	int b = 35;
+	
+	//与えられた小文字のアルファベットに対し、右隣のアルファベットを出力することができること
+	@Test
+	public void testShiftOnce() {
+
+	}
+
+	//与えられた小文字のアルファベットに対して、決まった数右隣のアルファベットを出力することができること
+	@Test
+	public void testShiftMutiple() {
+
+	}
+	
+	//与えられた平文に対して、暗号化鍵に従って、暗号文を出力できること
+	@Test
+	public void testEnc() {
+
+	}
+	
+	//暗号化鍵に対して、適切に復号化鍵を出力できること
+	@Test
+	public void testGenKey() {
+
+	}
+	
+	//暗号文に対して、復号化鍵に従って、平文を出力できること。
+	@Test
+	public void testDec() {
+
+	}
+	
+}
